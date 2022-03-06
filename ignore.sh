7883f706f3f692619df08219a5ca17d32bace87f
luoxh <luoxh@iMac.lan>
2022-03-06 18:33:10 +0800

    quick commit 2022-03-06 18:33:10

---
+++ ignore.sh
@@ -0,0 +1,4 @@
+git rm -r --cached .
+git add .
+git commit -m "quich update .gitignore $(date "+%Y-%m-%d %H:%M:%S")"
+git push
---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "quick commit $(date "+%Y-%m-%d %H:%M:%S")"
+git push
